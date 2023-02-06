5d2597a4084cdc356c536db210a2a45910f862ca
Yungxin <101496626+shinyungxin@users.noreply.github.com>
2023-02-06 22:28:37 +0800

    [new] integrated with web3 contract

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-// import { useStateContext } from "../context";
+import { useStateContext } from "../context";
 import { CustomButton } from "./";
 import { logo, menu, search, thirdweb } from "../assets";
 import { navlinks } from "../constants";
@@ -10,8 +10,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
-  // const { connect, address } = useStateContext();
-  const address = "0xabcde";
+  const { connect, address } = useStateContext();
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
@@ -45,7 +44,7 @@ const Navbar = () => {
         <Link to="/profile">
           <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img
-              src={thirdweb}
+              src={logo}
               alt="user"
               className="w-[60%] h-[60%] object-contain"
             />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,8 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 import App from "./App";
-import './index.css';
+import "./index.css";
+import { StateContextProvider } from "./context";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -12,7 +13,9 @@ const root = createRoot(container);
 root.render(
   <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <Router>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </Router>
   </ThirdwebProvider>
 );
