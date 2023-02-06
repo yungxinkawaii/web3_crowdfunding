ebb0f2606855bacd4e3fddec8febc00840d6dec7
Yungxin <101496626+shinyungxin@users.noreply.github.com>
2023-02-06 23:14:46 +0800

    [new] Homepage features

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -39,22 +39,51 @@ export const StateContextProvider = ({ children }) => {
     }
   };
 
+  const getCampaigns = async () => {
+    const campaigns = await contract.call("getCampaigns");
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+
+    return parsedCampaings;
+  };
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+
+    return filteredCampaigns;
+  };
+
   return (
     <StateContext.Provider
-      value={{ 
+      value={{
         address,
         contract,
         connect,
         createCampaign: publishCampaign,
-        // getCampaigns,
-        // getUserCampaigns,
+        getCampaigns,
+        getUserCampaigns,
         // donate,
         // getDonations
       }}
     >
       {children}
     </StateContext.Provider>
-  )
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
