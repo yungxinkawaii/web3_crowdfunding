b5e320c7d92f1e4aba51df3076203de36dbb3c47
Yungxin <101496626+shinyungxin@users.noreply.github.com>
2023-02-06 20:29:41 +0800

    [mod] disabled original src and created new src

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
       <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
+    </Router>
+  </ThirdwebProvider>
 );
